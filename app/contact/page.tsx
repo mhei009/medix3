fa021708def23ccdb2fbed69d1c1dfa43b242ef7
Heidi <emhp009@gmail.com>
2024-09-05 18:34:44 +0200

    contact done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,9 +7,11 @@ const Contact = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col md:flex-row h-screen max-h-screen">
-        <section className="remove-scrollbar container my-auto md:w-1/2 px-4">
-          <div className="sub-container max-w-[496px] mx-auto">
+      {/* Main container with scrolling enabled */}
+      <div className="flex flex-col md:flex-row min-h-screen md:h-screen overflow-auto pt-20 md:pt-0">
+        {/* Contact Information Section */}
+        <section className="container mx-auto p-4 md:w-1/2 flex flex-col justify-center">
+          <div className="max-w-[496px] mx-auto">
             <h1 className="text-2xl font-bold mb-4 text-blue-500">
               Contact <span className="text-green-500">Medix</span>
             </h1>
@@ -31,19 +33,19 @@ const Contact = () => {
             </div>
 
             <div className="text-16-regular mt-20 flex justify-between">
-              <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Medix</p>
+              <p className="text-dark-600 xl:text-left">© 2024 Medix</p>
             </div>
           </div>
         </section>
 
         {/* Google Map Section */}
-        <div className="md:w-1/2 px-4 flex items-center justify-center">
+        <div className="container mx-auto p-4 md:w-1/2 flex items-center justify-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d108938.63939242373!2d74.27457640628536!3d31.432512534029463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391906ffbed5423f%3A0xe42acafa4763a61f!2sAwan%20Market%2C%20Lahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2sse!4v1725552001456!5m2!1sen!2sse"
             width="100%"
-            height="450"
+            height="400"
             style={{ border: 0 }}
-            allowFullScreen=""
+           
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             className="rounded-lg shadow-md"
